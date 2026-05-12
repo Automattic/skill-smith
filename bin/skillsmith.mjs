a249babab68e8dbaab12a356e1bfca1c944fc342
Mario Santos <34552881+SantosGuillamot@users.noreply.github.com>
2026-05-12 10:13:28 +0200

    Add support for .env variables

--- bin/skillsmith.mjs
+++ bin/skillsmith.mjs
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
+import { loadEnvFile } from "node:process";
+
+try {
+	loadEnvFile();
+} catch (err) {
+	if (err?.code !== "ENOENT") throw err;
+}
+
 import "tsx/esm";
 
 const { run } = await import("../src/runner.ts");
